06f71de4f6effee824785432647ffa80042a5985
cchung <cchung@avilon.co>
2023-02-27 12:39:04 +0800

    stable v1

--- oil_app.py
+++ oil_app.py
@@ -4,9 +4,9 @@ from lib.OilPieChart import PieChart
 if __name__ == "__main__":
     root = tk.Tk()
     oil_list = OilDataList(root)
-    oil_list.pack()
+    oil_list.pack(side=tk.RIGHT)
     pie_chart = PieChart(root,oil_list,"功能比例")
-    pie_chart.pack()
+    pie_chart.pack(side=tk.RIGHT)
     root.mainloop()
 
 
